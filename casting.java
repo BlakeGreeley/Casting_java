fee9fd2abc6edfcc26dd4f99706b1ab96f87fc96
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-05-06 12:03:05 -0700

    Add files via upload

---
+++ casting.java
@@ -0,0 +1,19 @@
+class casting {
+    public static void main(String[] args) {
+        //double d = 35.25;
+        //double dd = 35.67;
+
+        //int i = (int) d;
+
+        //int ii = (int) dd;
+        //System.out.println(i);
+        //System.out.println(ii);
+        // these outputted 35 and 35.
+
+        int i = 35;
+        float f = i;
+
+    System.out.println("The number is: " + f);
+    }
+
+}
